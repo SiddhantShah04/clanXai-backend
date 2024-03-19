759d63158f7fbbd3753d413537b13f3e7716e097
SiddhantShah04 <siddhantshah04@gmail.com>
2024-03-19 07:34:32 +0530

    removed commented code

--- src/services/portpolio.service.ts
+++ src/services/portpolio.service.ts
@@ -1,8 +1,3 @@
-// import { CreateUserDto } from '@dtos/users.dto';
-// import { HttpException } from '@exceptions/HttpException';
-// import { User } from '@interfaces/users.interface';
-// import userModel from '@models/users.model';
-// import { isEmpty } from '@utils/util';
 import { PortfolioModel } from '../models/Portfolio.model';
 
 class PortpolioService {
